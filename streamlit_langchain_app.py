da96c19ca7cf8ad7f87286d5e984f1719f33f728
deloitte-kiran <111056381+kirankkirankumar@users.noreply.github.com>
2023-08-24 20:57:42 +0530

    Update streamlit_langchain_app.py

--- streamlit_langchain_app.py
+++ streamlit_langchain_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-from langchain_library import LangChainApp
+import openai
+
+# Set up your OpenAI API key
+openai.api_key = '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'
+
+class LangChainApp:
+    def analyze_text(self, user_input):
+        # Generate Mermaid.js code using GPT-3
+        prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=200
+        )
+        mermaid_code = response.choices[0].text.strip()
+        return mermaid_code
 
 def main():
     st.title("LangChain Application")
@@ -9,7 +24,8 @@ def main():
     user_input = st.text_input("Enter your text:")
     if st.button("Analyze"):
         result = langchain_app.analyze_text(user_input)
-        st.write("Analysis Result:", result)
+        st.code(result, language="mermaid")
 
 if __name__ == "__main__":
     main()
+
