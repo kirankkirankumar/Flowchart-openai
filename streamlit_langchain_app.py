98d89ab93a6b5261f00bc4c332ba157428d0f86e
deloitte-kiran <111056381+kirankkirankumar@users.noreply.github.com>
2023-08-24 21:03:17 +0530

    Update streamlit_langchain_app.py

--- streamlit_langchain_app.py
+++ streamlit_langchain_app.py
@@ -1,31 +1,19 @@
-import streamlit as st
 import openai
 
 # Set up your OpenAI API key
 openai.api_key = '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'
 
-class LangChainApp:
-    def analyze_text(self, user_input):
-        # Generate Mermaid.js code using GPT-3
-        prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=200
-        )
-        mermaid_code = response.choices[0].text.strip()
-        return mermaid_code
+# Collect user input
+user_input = "Describe the process of withrdrawing money in ATM if amount is available in account otherwise throw an error"
 
-def main():
-    st.title("LangChain Application")
-    langchain_app = LangChainApp()
+# Generate Mermaid.js code using GPT-3
+prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=prompt,
+    max_tokens=200
+)
+mermaid_code = response.choices[0].text.strip()
 
-    # Streamlit UI elements
-    user_input = st.text_input("Enter your text:")
-    if st.button("Analyze"):
-        result = langchain_app.analyze_text(user_input)
-        st.code(result, language="mermaid")
-
-if __name__ == "__main__":
-    main()
-
+# Print the generated Mermaid.js code
+print(mermaid_code)
