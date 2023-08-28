0c7c9f6ddedae0ec8fb347bd2c80bd32a3308006
deloitte-kiran <111056381+kirankkirankumar@users.noreply.github.com>
2023-08-28 11:51:01 +0530

    Update streamlit_langchain_app.py

--- streamlit_langchain_app.py
+++ streamlit_langchain_app.py
@@ -1,34 +1,41 @@
 
 import streamlit as st
 import openai
+import secrets
 import base64
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain, SequentialChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks import get_openai_callback
+import time 
+import logging
 
 #llm_model = OpenAI(temperature=0.9)
 
 # Set up your OpenAI API key
 openai.api_key = os.env.["OPEN_AI_KEY"]
 
-# Prompt templates
+# Set up logging
+logging.basicConfig(level=logging.INFO)  # Adjust the logging level as needed
+
+
+
 
 
 class LangChainApp:
-    try:
-        def analyze_text(self, user_input):
+    def analyze_text(self, user_input):
         # Generate Mermaid.js code using GPT-3
-            prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
-            response = openai.Completion.create(
+        prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
+        # Log the prompt
+        logging.info("Prompt: %s", prompt)
+        response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
             max_tokens=200
-            )
-            mermaid_code = response.choices[0].text.strip()
-            return mermaid_code
-    except openai.error.OpenAIError as e:
-        st.error("Error generating steps: " + str(e))
+        )
+        mermaid_code = response.choices[0].text.strip()
+        return mermaid_code
+    
 # Collect user input
 user_input = "Describe the process of withrdrawing money in ATM if amount is available in account otherwise throw an error"
 
@@ -37,6 +44,7 @@ def main():
     langchain_app = LangChainApp()
 
     # Streamlit UI elements
+    start_time = time.time()
     user_input = st.text_input("Enter your text:")
     if st.button("Analyze"):
         result = langchain_app.analyze_text(user_input)
@@ -48,16 +56,9 @@ def main():
         base64_bytes = base64.b64encode(graphbytes)
         base64_string = base64_bytes.decode("ascii")
         st.image("https://mermaid.ink/img/" + base64_string)
-     
-
-if __name__ == "__main__":
-    main()
-
-    # Streamlit UI elements
-    user_input = st.text_input("Enter your text:")
-    if st.button("Analyze"):
-        result = langchain_app.analyze_text(user_input)
-        st.code(result, language="mermaid")
+        end_time = time.time()  # Record the end time
+        time_taken = end_time - start_time  # Calculate time taken
+        logging.info("time taken"+str(time_taken))
 
 if __name__ == "__main__":
     main()
