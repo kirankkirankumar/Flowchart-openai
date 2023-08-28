0805811a1ccd81eb172f7c090fdadac586614e1b
deloitte-kiran <111056381+kirankkirankumar@users.noreply.github.com>
2023-08-28 11:10:06 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,22 +1,35 @@
+
 import streamlit as st
 import openai
 import secrets
+import base64
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain, SequentialChain
+from langchain.prompts import PromptTemplate
+from langchain.callbacks import get_openai_callback
 
+#llm_model = OpenAI(temperature=0.9)
 
 # Set up your OpenAI API key
-openai.api_key = secerts.openaikey.OPEN_API_KEY
+openai.api_key = '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'
+
+# Prompt templates
+
 
 class LangChainApp:
-    def analyze_text(self, user_input):
+    try:
+        def analyze_text(self, user_input):
         # Generate Mermaid.js code using GPT-3
-        prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
-        response = openai.Completion.create(
+            prompt = f"Generate Mermaid.js code for a flowchart: {user_input}"
+            response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
             max_tokens=200
-        )
-        mermaid_code = response.choices[0].text.strip()
-        return mermaid_code
+            )
+            mermaid_code = response.choices[0].text.strip()
+            return mermaid_code
+    except openai.error.OpenAIError as e:
+        st.error("Error generating steps: " + str(e))
 # Collect user input
 user_input = "Describe the process of withrdrawing money in ATM if amount is available in account otherwise throw an error"
 
@@ -28,7 +41,15 @@ def main():
     user_input = st.text_input("Enter your text:")
     if st.button("Analyze"):
         result = langchain_app.analyze_text(user_input)
-        st.code(result, language="mermaid")
+        #st.code(result, language="mermaid")
+        st.text(result)
+        start_index = result.index("graph")
+        extracted_content = result[start_index:]
+        graphbytes = extracted_content.encode("ascii")
+        base64_bytes = base64.b64encode(graphbytes)
+        base64_string = base64_bytes.decode("ascii")
+        st.image("https://mermaid.ink/img/" + base64_string)
+     
 
 if __name__ == "__main__":
     main()
